a68f00674cfb5670b16bddef4e3d05b826392405
Jun Aishima <jaishima@bnl.gov>
2026-04-03 13:45:42 -0400

    prevent passing API key read from environment file
     * read key and use it right before the Tiled client is created,
       so there is no need to pass it around
     * it is still possible to run workflows locally or in CI by providing
       the API key in `end_of_run_workflow()` but Prefect Cloud will not
       be involved, so the key value will not be visible

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,14 +1,5 @@
-import os
 from prefect import task, flow, get_run_logger
 from data_validation import data_validation
-from dotenv import load_dotenv
-
-
-def get_api_key_from_env(api_key=None):
-    with open("/srv/container.secret", "r") as secrets:
-        load_dotenv(stream=secrets)
-    api_key = os.environ["TILED_API_KEY"]
-    return api_key
 
 
 @task
@@ -20,8 +11,6 @@ def log_completion(dry_run=False):
 @flow
 def end_of_run_workflow(stop_doc, api_key=None, dry_run=False):
     uid = stop_doc["run_start"]
-    if not api_key:
-        api_key = get_api_key_from_env(api_key=None)
     data_validation(uid, api_key=api_key, dry_run=dry_run)
     log_completion(dry_run=dry_run)
     return True
