9166196c11112225044ae78aae2bac5d4c83f001
kari Barry <kezzsim@gmail.com>
2026-04-13 13:39:29 -0400

    🌎️ Define the core requirements

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -2,7 +2,10 @@ import os
 from prefect import task, flow, get_run_logger
 from data_validation import data_validation
 from dotenv import load_dotenv
-
+# Application Specific
+from tiled.client import from_uri
+from xas.process import process_interpolate_bin_with_tiled
+tiled_inst = "https://tiled.nsls2.bnl.gov"
 
 def get_api_key_from_env(api_key=None):
     with open("/srv/container.secret", "r") as secrets:
@@ -23,5 +26,9 @@ def end_of_run_workflow(stop_doc, api_key=None, dry_run=False):
     if not api_key:
         api_key = get_api_key_from_env(api_key=None)
     data_validation(uid, api_key=api_key, dry_run=dry_run)
+    # Processing goes here
+    reading_client = from_uri(tiled_inst, api_key=api_key)[f"qas/raw/{uid}"]
+    writing_client = from_uri(tiled_inst, api_key=api_key)[f"qas/processed/{uid}"]
+    process_interpolate_bin_with_tiled(reading_client, writing_client)
     log_completion(dry_run=dry_run)
     return True
