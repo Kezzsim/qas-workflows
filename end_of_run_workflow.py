92d88706320c1b7322ded5097da8411a09de3ae5
Jun Aishima <jaishima@bnl.gov>
2026-04-29 16:19:32 -0400

    consolidate Tiled clients into data_validation

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,8 +1,7 @@
 from prefect import task, flow, get_run_logger
-from data_validation import data_validation
+from data_validation import data_validation, get_run, get_run_processed
 
 # QAS Application Specific
-from tiled.client import from_uri
 from xas.process import process_interpolate_bin_with_tiled
 
 tiled_inst = "https://tiled.nsls2.bnl.gov"
@@ -18,9 +17,10 @@ def end_of_run_workflow(stop_doc, api_key=None, dry_run=False):
     uid = stop_doc["run_start"]
     data_validation(uid, api_key=api_key, dry_run=dry_run)
     # Processing goes here
-    client = from_uri(tiled_inst, api_key=api_key)
+    run = get_run(uid)
+    run_processed = get_run_processed(uid)
     process_interpolate_bin_with_tiled(
-        client[f"qas/raw/{uid}"], client[f"qas/processed/{uid}"]
+        run, run_processed
     )
     log_completion(dry_run=dry_run)
     return True
