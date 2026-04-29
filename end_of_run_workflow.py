a4e6f67cec67963e43a06dff101c91eca1c137d2
kari Barry <kezzsim@gmail.com>
2026-04-29 11:16:23 -0400

    💃 Rebase Tango part 2

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,6 +1,11 @@
 from prefect import task, flow, get_run_logger
 from data_validation import data_validation
 
+# Application Specific
+from tiled.client import from_uri
+from xas.process import process_interpolate_bin_with_tiled
+
+tiled_inst = "https://tiled.nsls2.bnl.gov"
 
 @task
 def log_completion(dry_run=False):
@@ -12,5 +17,10 @@ def log_completion(dry_run=False):
 def end_of_run_workflow(stop_doc, api_key=None, dry_run=False):
     uid = stop_doc["run_start"]
     data_validation(uid, api_key=api_key, dry_run=dry_run)
+    # Processing goes here
+    client = from_uri(tiled_inst, api_key=api_key)
+    process_interpolate_bin_with_tiled(
+        client[f"qas/raw/{uid}"], client[f"qas/processed/{uid}"]
+    )
     log_completion(dry_run=dry_run)
     return True
